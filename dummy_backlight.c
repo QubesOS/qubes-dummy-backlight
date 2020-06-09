446ae5f5ef2768649d64d2e56dc922cd8d6b394b
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-06-09 18:21:52 +0200

    Initial commit
    QubesOS/qubes-issues#4186

---
+++ dummy_backlight.c
@@ -0,0 +1,68 @@
+#include <linux/delay.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/fb.h>
+#include <linux/backlight.h>
+
+static int curr_intensity;
+static struct backlight_device *dummy_backlight_device;
+
+static bool module_initialized;
+
+static int set_intensity(struct backlight_device *bd)
+{
+	curr_intensity = bd->props.brightness;
+	return 0;
+}
+
+static int get_intensity(struct backlight_device *bd)
+{
+	return curr_intensity;
+}
+
+static const struct backlight_ops dummy_ops = {
+	.options = BL_CORE_SUSPENDRESUME,
+	.get_brightness = get_intensity,
+	.update_status = set_intensity,
+};
+
+static int __init dummy_backlight_init(void)
+{
+	struct backlight_properties props;
+	const char *name = "DUMMY_BL";
+
+	memset(&props, 0, sizeof(struct backlight_properties));
+	props.type = BACKLIGHT_RAW;
+	props.max_brightness = 100;
+
+	dummy_backlight_device =
+		backlight_device_register(name, NULL, NULL, &dummy_ops, &props);
+
+	if (IS_ERR(dummy_backlight_device))
+		return PTR_ERR(dummy_backlight_device);
+
+	dummy_backlight_device->props.power = FB_BLANK_UNBLANK;
+	dummy_backlight_device->props.brightness = 90;
+	backlight_update_status(dummy_backlight_device);
+
+	module_initialized = true;
+	return 0;
+}
+module_init(dummy_backlight_init);
+
+static void __exit dummy_backlight_exit(void)
+{
+	dummy_backlight_device->props.power = 0;
+	dummy_backlight_device->props.brightness = 0;
+	backlight_update_status(dummy_backlight_device);
+	ssleep(3);
+	backlight_device_unregister(dummy_backlight_device);
+
+	module_initialized = false;
+}
+module_exit(dummy_backlight_exit);
+
+MODULE_AUTHOR("Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>");
+MODULE_DESCRIPTION("Dummy backlight Driver");
+MODULE_LICENSE("GPL");
